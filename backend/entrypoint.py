149eaa392e262ada4918d322bd36ca797fc1a477
Xavier Carey <xmancarey31@gmail.com>
2024-10-26 21:27:27 +1100

    wrote basic database tests, modified entrypoint to run them

--- backend/entrypoint.py
+++ backend/entrypoint.py
@@ -1,3 +1,5 @@
+import unittest
+from database_test import database_tests
 from database.database import initdb
 from app import run_server
 from logger_config import logger
@@ -8,5 +10,8 @@ if __name__ == "__main__":
     
     # Initialize the database
     initdb()
+
+    # runner = unittest.TextTestRunner()
+    # runner.run(database_tests())
 
     run_server()
