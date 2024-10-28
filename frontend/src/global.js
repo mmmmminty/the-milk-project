208d74cb19f4937fb56876566fd8fd03b5838fe9
Zitian Qin <z5476244@ad.unsw.edu.au>
2024-10-28 22:00:31 +1100

    Add js logic for login

--- frontend/src/global.js
+++ frontend/src/global.js
@@ -1,2 +1,22 @@
 import { BACKEND_PORT } from "./config";
 import { isUserLoggedIn, setToken } from "./util";
+import { login } from "./api";
+
+document
+  .getElementById("login-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const data = await login(username, password);
+
+    if (data.token) {
+      setToken(data.token);
+    }
+
+    if (isUserLoggedIn()) {
+      window.location.href = "/dashboard";
+    }
+  });
