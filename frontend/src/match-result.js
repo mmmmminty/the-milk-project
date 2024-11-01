204c01f19250998d70672de234d806f809c6b264
Trixie Vicgor Limanto <z5465113@ad.unsw.edu.au>
2024-11-01 16:34:56 +1100

    apiCall patient on work

--- frontend/src/match-result.js
+++ frontend/src/match-result.js
@@ -3,7 +3,27 @@ document.getElementById('back-match').addEventListener('click', () => {
 	window.location.href = 'scanner.html';
 });
 
+// Provide milk info
 apiCall(`milk?id=${milkId}`, 'GET', {}, token).then(milkInfo => {
+    document.querySelector('.baby-name').textContent = "Baby's Full Name: ";
+    document.querySelector('.express-date').textContent = "Express Date: " + milkInfo.Expressed;
+    document.querySelector('.storing-type').textContent = "Storing Type: " + 
+        (milkInfo.Frozen ? "Frozen" : "Fridge") + 
+        (milk.Defrosted ? " (defrosted)" : " (chilled)");
+    document.querySelector('.expiry-date').textContent = "Expiry Date: " + milkInfo.Expiry;
+
+    const nutrientsList = document.querySelector('.nutrients-list');
+    const nutrients = milkInfo.Additives;
+    nutrients.forEach(nutrient => {
+        const listItem = document.createElement('li');
+        listItem.textContent = nutrient;
+        nutrientsList.appendChild(listItem);
+    });
+})
+
+// Provide patient info
+apiCall(`milk?id=${milkId}`, 'GET', {}, token).then(milkInfo => {
+    document.getElementById('milk').textContent = "Milk's Code #" + milkId;
     document.querySelector('.baby-name').textContent = "Baby's Full Name: ";
     document.querySelector('.express-date').textContent = "Express Date: " + milkInfo.Expressed;
     document.querySelector('.storing-type').textContent = "Storing Type: " + 
