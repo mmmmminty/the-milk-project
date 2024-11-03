284ceb85df81466904cc1e614db3f1011f5d50f1
Darren Andijaya <darren.andijaya@gmail.com>
2024-11-04 01:27:41 +1100

    fiixed metrge issue

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -4,11 +4,9 @@ from database.tables.staff import create_nurse, fetch_nurse, link_nurse_to_baby,
 from database.tables.additives import add_additive_to_milk, fetch_additives, create_additive, fetch_all_additives, fetch_additive_by_name, update_additive_expiry_modifier
 from database.tables.family import create_mother_and_baby, create_baby, delete_family, fetch_mothers, fetch_mother, fetch_all_babies, fetch_babies, fetch_baby, delete_family
 from utils.label_maker import label_maker
-<<<<<<< HEAD
 from database.validation import validate, ValidationType 
-=======
 from utils.qr_code import baby_qr_code_maker
->>>>>>> 3a1f71a (wrote route for baby qr)
+
 import os
 
 bp = Blueprint('routes', __name__)
