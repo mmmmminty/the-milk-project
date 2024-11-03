2ebd26d5e03cc6bd165062d3dc6439a70eb70da0
Darren Andijaya <darren.andijaya@gmail.com>
2024-11-04 01:27:41 +1100

    fiixed metrge issue

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -5,6 +5,8 @@ from database.tables.additives import add_additive_to_milk, fetch_additives, cre
 from database.tables.family import create_mother_and_baby, create_baby, delete_family, fetch_mothers, fetch_mother, fetch_all_babies, fetch_babies, fetch_baby, delete_family
 from utils.label_maker import label_maker
 from database.validation import validate, ValidationType 
+
+
 import os
 
 bp = Blueprint('routes', __name__)
