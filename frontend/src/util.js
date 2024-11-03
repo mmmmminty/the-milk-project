6678d2931ac3e8933d682ca5c4fa1728f23d1724
Zitian Qin <z5476244@ad.unsw.edu.au>
2024-11-03 17:28:19 +1100

    Refactor api.js and add getMilk function

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,7 +1,8 @@
 import { BACKEND_PORT } from "./config";
 
-// TODO: Use in-memory storage for token
-export function isValidToken(token) {}
+export function getToken() {
+  return localStorage.getItem("token");
+}
 
 export function setToken(token) {
   localStorage.setItem("token", token);
